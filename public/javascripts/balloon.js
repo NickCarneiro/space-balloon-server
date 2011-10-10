40fa47ceaf4efae23a141cf33b5d72a311768c76
Nick Carneiro <burthawk101@gmail.com>
2011-10-10 00:54:26 -0500

    added marker basics

--- public/javascripts/balloon.js
+++ public/javascripts/balloon.js
@@ -1,5 +1,6 @@
 var balloon = {};
 balloon.initialize = function() {
+	
     var latlng = new google.maps.LatLng(-34.397, 150.644);
     var myOptions = {
       zoom: 8,
@@ -7,6 +8,12 @@ balloon.initialize = function() {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    var marker = new google.maps.Marker({
+      position: latlng, 
+      map: map, 
+      title:"Hello World!"
+  	});   
 }
 
 $(function(){
